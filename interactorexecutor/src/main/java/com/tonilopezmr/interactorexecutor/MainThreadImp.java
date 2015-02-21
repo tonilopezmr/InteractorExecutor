0a12df7317116b64530268817acdc930bcf61634
Antonio López Marín <tonilopezmr@gmail.com>
2015-02-21 13:55:13 +0100

    Add source code

---
+++ interactorexecutor/src/main/java/com/tonilopezmr/interactorexecutor/MainThreadImp.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 Jorge Castillo Pérez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tonilopezmr.interactorexecutor;
+
+import android.os.Handler;
+import android.os.Looper;
+
+/**
+ * {@link MainThread} implementation. Will make
+ * interactor Callbacks able to get executed in the Android UI thread*
+ *
+ *
+ * @author Antonio López
+ */
+public class MainThreadImp implements MainThread {
+
+    private Handler handler;
+
+    public MainThreadImp() {
+        this.handler = new Handler(Looper.getMainLooper());
+    }
+
+    @Override
+    public void post(Runnable runnable) {
+        handler.post(runnable);
+    }
+}
---
+++ interactorexecutor/src/main/java/com/tonilopezmr/interactorexecutor/ThreadExecutor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015 Antonio López Marín <tonilopezmr.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tonilopezmr.interactorexecutor;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Interactor dispatcher. Contains the initial executor setup.
+ * Using {@link java.util.concurrent.ThreadPoolExecutor} as the executor implementation.
+ *
+ *
+ * @author Antonio López
+ */
+public class ThreadExecutor implements Executor{
+
+    private static final int CORE_POOL_SIZE = 3;
+    private static final int MAX_POOL_SIZE = 5;
+    private static final int KEEP_ALIVE_TIME = 120;
+    private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+    private static final BlockingQueue<Runnable> WORK_QUEUE = new LinkedBlockingQueue();
+
+    private ThreadPoolExecutor threadPoolexecutor;
+
+    public ThreadExecutor() {
+        int corePoolSize = CORE_POOL_SIZE;
+        int maxPoolSize = MAX_POOL_SIZE;
+        int keepAliveTime = KEEP_ALIVE_TIME;
+        TimeUnit timeUnit = TIME_UNIT;
+        BlockingQueue<Runnable> workQueue = WORK_QUEUE;
+
+        threadPoolexecutor =
+                new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, timeUnit, workQueue);
+    }
+
+    @Override
+    public void run(final Interactor interactor) {
+        if (interactor == null) {
+            throw new IllegalArgumentException("Interactor to execute can't be null");
+        }
+
+        threadPoolexecutor.submit(interactor);
+    }
+
+
+}
